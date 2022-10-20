770ff2e6b026d71ce283c37838f7d8710cbdb27d
vikash kumar <krvikash35@gmail.com>
2022-10-20 10:21:16 +0530

    add python error handling

--- l-python/playground/main.py
+++ l-python/playground/main.py
@@ -1,38 +1,22 @@
-import json
+class MyError(Exception):
+    pass
+
+def f1():
+    print("f1")
+    raise MyError("my error")
+
+def f2():
+    print("f2")
+    f1()
 
 
-data = {
-    "cities1": [{
-        "name": "n1",
-        "state": "s1"
-    }, {"name": "n1",
-        "state": "s1"
-        }]
-}
+def f3():
+    print("f3")
+    f2()
 
 
-
-# for f in data["cities"]:
-#     print(f)
-# b ="cities" in data and ['[f for f in data.get("cities")]
-b = [f for f in data["cities"]] if "cities" in data else []
-print(b)
-
-a = {
-
-}
-print(a.get("name"))
-# print(a["name"])
-
-o1 = {
-    "name": "vikash",
-    "age": None
-}
-
-print(o1)
-print(json.dumps(o1))
-
-o2 = json.loads(json.dumps(o1))
-print(o2)
-print(o2["name"])
-print(o2["age"])
+try:
+    f3()
+except Exception as e:
+    print("caught")
+    raise Exception("chain Exception").with_traceback(e.__traceback__)
