c022d51b3bed14e3f70341262b262d479840a88e
vikash kumar <krvikash35@gmail.com>
2022-10-21 03:48:20 +0530

    add kubernetes metric

--- l-python/playground/main.py
+++ l-python/playground/main.py
@@ -1,22 +1,27 @@
-class MyError(Exception):
-    pass
+# class MyError(Exception):
+#     pass
 
-def f1():
-    print("f1")
-    raise MyError("my error")
+# def f1():
+#     print("f1")
+#     raise MyError("my error")
 
-def f2():
-    print("f2")
-    f1()
-
-
-def f3():
-    print("f3")
-    f2()
+# def f2():
+#     print("f2")
+#     f1()
 
 
-try:
-    f3()
-except Exception as e:
-    print("caught")
-    raise Exception("chain Exception").with_traceback(e.__traceback__)
+# def f3():
+#     print("f3")
+#     f2()
+
+
+# try:
+#     f3()
+# except Exception as e:
+#     print("caught")
+#     raise Exception("chain Exception").with_traceback(e.__traceback__)
+
+ticket_tags="aaaduplicatefff"
+
+a =ticket_tags.find("duplicate") != -1
+print(a)
